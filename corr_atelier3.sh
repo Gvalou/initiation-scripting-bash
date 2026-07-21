ec7271612b45a67bece854421e701e11d6b01592
Gvalou <valentin.palamara@hotmail.com>
2026-07-21 12:00:06 +0200

    Create corr_atelier3.sh to analyze text files
    This script displays the working directory, lists text files, and shows characteristics of a selected file.

---
+++ corr_atelier3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#==========
+#auteur : valentin
+#date de création : 21/06/26
+#dernière modif :
+#==========
+
+rep=$PWD
+extension=txt
+
+echo "répertoire de travail : $rep"
+echo "fichiers pouvant être traités : "
+ls $rep/*.$extension
+
+#choix du fichier à traiter
+echo ""
+echo "Quel fichier voulez-vous traiter ?"
+read fic trash
+nblign=$(wc -l $fic)
+debut=$(head -n 2 $fic)
+fin=$(tail -n 2 $fic)
+
+#affichage des caractéristiques du fichier
+echo "CARACTERISTIQUES de $fic"
+echo "Nombre de ligne du fichier : $nblign"
+echo "Début du fichier : "
+echo $debut
+echo "Fin du fichier : "
+echo $fin
